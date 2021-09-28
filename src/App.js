f76da07ba13d7b0954b23d36d191b1f8757bd58d
Muhammad Nurul Ahsan <86627956+Muhammadnurulahsan21@users.noreply.github.com>
2021-09-28 16:01:22 +0600

    neww code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Card from "./Components/Card/Card";
 
 function App() {
+  const items = [
+    {
+      name: "World's No-1 Image",
+      description:
+        "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+      img: "https://i.pinimg.com/originals/99/83/cf/9983cf8edddae80ed2b9599a26111bc7.jpg",
+    },
+    {
+      name: "World's No-1 Image",
+      description:
+        "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+      img: "https://i.pinimg.com/originals/99/83/cf/9983cf8edddae80ed2b9599a26111bc7.jpg",
+    },
+    {
+      name: "World's No-1 Image",
+      description:
+        "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+      img: "https://i.pinimg.com/originals/99/83/cf/9983cf8edddae80ed2b9599a26111bc7.jpg",
+    },
+    {
+      name: "World's No-1 Image",
+      description:
+        "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+      img: "https://i.pinimg.com/originals/99/83/cf/9983cf8edddae80ed2b9599a26111bc7.jpg",
+    },
+    {
+      name: "World's No-1 Image",
+      description:
+        "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+      img: "https://i.pinimg.com/originals/99/83/cf/9983cf8edddae80ed2b9599a26111bc7.jpg",
+    },
+    {
+      name: "World's No-1 Image",
+      description:
+        "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+      img: "https://i.pinimg.com/originals/99/83/cf/9983cf8edddae80ed2b9599a26111bc7.jpg",
+    },
+    {
+      name: "World's No-1 Image",
+      description:
+        "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+      img: "https://i.pinimg.com/originals/99/83/cf/9983cf8edddae80ed2b9599a26111bc7.jpg",
+    },
+    {
+      name: "World's No-1 Image",
+      description:
+        "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+      img: "https://i.pinimg.com/originals/99/83/cf/9983cf8edddae80ed2b9599a26111bc7.jpg",
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button type="button" className="btn btn-primary">
+        Primary
+      </button>
+      <button type="button" className="btn btn-secondary">
+        Secondary
+      </button>
+      <button type="button" className="btn btn-success">
+        Success
+      </button>
+      <button type="button" className="btn btn-danger">
+        Danger
+      </button>
+      <button type="button" className="btn btn-warning">
+        Warning
+      </button>
+      <button type="button" className="btn btn-info">
+        Info
+      </button>
+      <button type="button" className="btn btn-dark">
+        Dark
+      </button>
+
+      <div className="row row-cols-1 row-cols-md-4 g-4">
+        {items.map((item) => (
+          <Card item={item}></Card>
+        ))}
+      </div>
     </div>
   );
 }
